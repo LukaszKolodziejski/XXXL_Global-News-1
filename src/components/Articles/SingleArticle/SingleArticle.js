36d26ae9f935315aeada96fc61486dc078d09b79
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-10 16:07:02 +0200

    styles SingleArticle, Articles,Filters and Layout

--- src/components/Articles/SingleArticle/SingleArticle.js
+++ src/components/Articles/SingleArticle/SingleArticle.js
@@ -1,24 +1,28 @@
 import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import styles from "./css/SingleArticle.module.css";
 class SingleArticle extends Component {
   state = {};
   render() {
     return (
       <Fragment>
-        <article>
-          <div className="image">IMAGE</div>
-          <div className="information">
-            <span className="information__data">Nov 29, 2018</span>
-            <span className="information__data">John Smith</span>
-            <span className="information__data">the Verge</span>
+        <article className={styles.SingleArticle}>
+          <img
+            className={styles.SingleArticle__image}
+            src="https://via.placeholder.com/150"
+          ></img>
+          <div className={styles.Information}>
+            <span className={styles.Information__data}>Nov 29, 2018</span>
+            <span className={styles.Information__data}>John Smith</span>
+            <span className={styles.Information__data}>the Verge</span>
           </div>
-          <h3 className="title">Title Article</h3>
-          <div className="description">
+          <h3 className={styles.SingleArticle__title}>Title Article</h3>
+          <div className={styles.SingleArticle__description}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
             quia error laudantium. Accusantium ullam facere tempore, voluptas
             rerum atque maxime.
           </div>
-          <Link to="/article" className="CTA">
+          <Link to="/article" className={styles.SingleArticle__CTA}>
             Read More
           </Link>
         </article>
