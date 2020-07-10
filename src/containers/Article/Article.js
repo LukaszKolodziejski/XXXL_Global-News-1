02fea47a3cf6e1fe07d23a1b29df494d36f00b55
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-10 16:37:15 +0200

    change ReturnButton on Article Page

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 
 import SingleArticle from "../../components/Articles/SingleArticle/SingleArticle";
 import Title from "../../components/Title/Title";
-
+import ReturnButton from "../../components/Navigation/ReturnButton/ReturnButton";
 class Article extends Component {
   state = {};
   render() {
     return (
       <Fragment>
         <Title name="Article Title" />
-        <Link to="/" className="returnIndex">
-          Return to articles list
-        </Link>
+        <ReturnButton>Return to articles list</ReturnButton>
         <SingleArticle />
       </Fragment>
     );
