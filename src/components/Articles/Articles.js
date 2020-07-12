77d34b4b18b75cabe0e1a6c45047ea6dceb98f69
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-12 18:04:15 +0200

    Articles.js show all article after loading

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -5,23 +5,39 @@ import SingleArticle from "./SingleArticle/SingleArticle";
 import Button from "../../components/UI/Button/Button";
 import styles from "./css/Articles.module.css";
 class Articles extends Component {
-  state = {};
+  state = {
+    countArticles: 6, // in Redux
+  };
 
   componentDidMount = () => this.props.onFetchArticles();
 
   render() {
+    const { countArticles } = this.state;
     const { loading, articles } = this.props;
-    let singleArticles;
+    let allArticles = [];
 
-    !loading
-      ? (singleArticles = <SingleArticle data={articles[2][0]} />)
-      : (singleArticles = <p>Loading ...</p>);
+    if (!loading) {
+      for (let key in articles[2]) {
+        key !== "id" &&
+          allArticles.push(<SingleArticle key={key} data={articles[2][key]} />);
+      }
+    } else {
+      allArticles = <p>Loading ...</p>;
+    }
+
+    //TODO: If clearButton -> countArticles = 6 and allArticles.empty()
+
+    // !loading
+    //   ? (allArticles = <SingleArticle data={articles[2][0]} />)
+    //   : (allArticles = <p>Loading ...</p>);
 
     return (
       <main className={styles.Articles}>
-        <div className={styles.Articles__Content}>{singleArticles}</div>
+        <div className={styles.Articles__Content}>{allArticles}</div>
         <div className={styles.Articles__Button}>
-          <Button btnType="LoadMoreButton">Show More</Button>
+          <Button btnType="LoadMoreButton" clicked={this.loadMoreButtonHandler}>
+            Show More
+          </Button>
         </div>
       </main>
     );
