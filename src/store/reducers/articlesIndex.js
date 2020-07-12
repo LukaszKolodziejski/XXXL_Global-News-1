58a0e892c8965f7a08c5854f5132eaa5ffc5e573
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-12 19:19:53 +0200

    add actions and reducers to Article.js

--- src/store/reducers/articlesIndex.js
+++ src/store/reducers/articlesIndex.js
@@ -1,26 +1,8 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-  status: "ok",
-  totalResults: 36591,
-  articles: [
-    {
-      source: {
-        id: null,
-        name: "Lifehacker.com",
-      },
-      author: "Elizabeth Yuko",
-      title: "How to Read a Trail Map",
-      description:
-        "Going on walks and hikes in uncrowded parks is one of the few activities that we can still do without having to worry too much about spreading COVID-19 (assuming that you’re either by yourself, with the people you’ve been quarantining with, or with other peop…",
-      url: "https://lifehacker.com/how-to-read-a-trail-map-1844344804",
-      urlToImage:
-        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/xynvcf94yciusntl6uly.jpg",
-      publishedAt: "2020-07-11T14:00:00Z",
-      content:
-        "Going on walks and hikes in uncrowded parks is one of the few activities that we can still do without having to worry too much about spreading COVID-19 (assuming that youre either by yourself, with t… [+3605 chars]",
-    },
-  ],
+  loading: true,
+  articles: [],
 };
 
 const fetchArticlesStart = (state, action) => ({
@@ -30,13 +12,13 @@ const fetchArticlesStart = (state, action) => ({
 
 const fetchArticlesSuccess = (state, action) => ({
   ...state,
-  loading: true,
+  loading: false,
   articles: action.articles,
 });
 
 const fetchArticlesFail = (state, action) => ({
   ...state,
-  loading: true,
+  loading: false,
 });
 
 const reducer = (state = initialState, action) => {
